ae68ad8650328da5f59386db14fc80da77400839
MR.Dev <marcorettoremattes@gmail.com>
2025-12-02 16:52:32 -0300

    triying to build

--- keyboards/bastardkb/charybdis/3x6/keymaps/vendor/rules.mk
+++ keyboards/bastardkb/charybdis/3x6/keymaps/vendor/rules.mk
@@ -1,1 +1,11 @@
 VIA_ENABLE = yes
+
+AUDIO_SUPPORTED = no        # Audio is not supported
+RGB_MATRIX_SUPPORTED = no # RGB matrix is supported and enabled by default
+RGBLIGHT_SUPPORTED = no # RGB underglow is supported, but not enabled by default
+
+SPLIT_KEYBOARD = yes
+
+POINTING_DEVICE_DRIVER = pmw3360
+
+SERIAL_DRIVER = vendor
